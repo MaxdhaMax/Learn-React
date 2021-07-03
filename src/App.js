ba21600c21ec1d11bb323912fa51c4a1f3228cb3
MaxdhaMax <maxdhamaxi@gmail.com>
2021-07-03 08:37:56 +0700

    belajar conditional rendering dengan use state

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import "./App.css";
 import Card from "./Card";
 
 function App() {
+  // state
   const [name, setName] = useState("max");
+  const [showCard, setShowCard] = useState(true);
+  // functions
   const changeName = (name) => setName(name);
   const changeInput = (event) => setName(event.target.value);
+  const toogleShowCard = () => setShowCard(!showCard);
+
   const buttonsMarkup = (
     <div>
       <button className="button button2">Blue</button>
       <button className="button button3">Red</button>
     </div>
   );
+  const cardMarkup = showCard && (
+    <Card name={name} title="International Response Architect avatar" onChangeName={() => changeName("alan")} onChangeInput={changeInput} avatar="https://cdn.fakercloud.com/avatars/terrorpixel_128.jpg">
+      {buttonsMarkup}
+    </Card>
+  );
 
   return (
     <div className="App">
-      <button className="button" onClick={() => changeName("John")}>
-        Change Name
+      <button className="button" onClick={toogleShowCard}>
+        show or hide
       </button>
-      <Card name={name} title="International Response Architect avatar" onChangeName={() => changeName("alan")} onChangeInput={changeInput} avatar="https://cdn.fakercloud.com/avatars/terrorpixel_128.jpg">
-        {buttonsMarkup}
-      </Card>
+      {cardMarkup}
     </div>
   );
 }
